cfcc6d630d59b48b94bcff7b088810c52fb22b9c
Vaulz <deagle7k@gmail.com>
2019-05-28 12:19:22 +0300

    Second func

--- FileSharing/FileSharing/Controllers/FileController.cs
+++ FileSharing/FileSharing/Controllers/FileController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using FileSharing.Context;
 using FileSharing.Models;
@@ -38,7 +39,21 @@ namespace FileSharing.Controllers
                 {
                     await uploadedFile.CopyToAsync(fileStream);
                 }
-                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path,ShortDescription = shortDescription,Description = description};
+
+
+                string Alphabet= "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+                Random rnd = new Random();
+                StringBuilder sb = new StringBuilder(5);
+                int Position = 0;
+                int Lenght = 6;
+                for (int i = 0; i < Lenght; i++)
+                {
+                    Position = rnd.Next(0, Alphabet.Length - 1);
+                    sb.Append(Alphabet[Position]);
+                }
+
+                file.ShortURL = sb.ToString();
                 _appContext.Files.Add(file);
                 _appContext.SaveChanges();
             }
@@ -48,7 +63,14 @@ namespace FileSharing.Controllers
 
         public ActionResult GetAllFiles()
         {
-            return View();
+            return View(_appContext.Files.ToList());
+        }
+
+       
+        public ActionResult GetFileProfile(int id)
+        {
+            var file= _appContext.Files.FirstOrDefault(x => x.Id == id);
+            return View(file);
         }
     }
 }
